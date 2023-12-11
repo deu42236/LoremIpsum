2259382d220a94b79d52803bc2ff56f2d72613a6
deu42236 <147750281+deu42236@users.noreply.github.com>
2023-12-11 17:56:32 +0100

    Verze 0.25 EN
    Porgram zohledňuje i četnost samohlásek, na začátek outputu dává "Lorem english"

--- lorem_English.py
+++ lorem_English.py
@@ -5,51 +5,51 @@ abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'
 cons = ['q', 'w', 'r', 't', 'p', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'x', 'c', 'v', 'b', 'n', 'm', 'th', 'he', 'an'] #cons... souhlasky
 cons_w = [9, 234, 568, 937, 166, 611, 414, 203, 192, 611, 23, 87, 424, 20, 273, 106, 154, 680, 253, 30, 30, 15]
 vowe = ['a', 'e', 'i', 'o', 'u', 'y'] #vowe... samohlasky
-vowe_w = [834, 1260, 671, 770, 285, 204]
-struktura = ["CV", "CVCV","CCV", "CVC", "CCVCC"]
+vowe_w = [834, 1260, 671, 770, 285, 304]
+structure = ["CV", "CVCV","CCV", "CVC", "CCVCC"]
 
-start = "Lorem English"
-word = ""
+output = "Lorem english "
         ## C = souhlaska, V = samohlaska
 
 
-struktura_now = random.choices(struktura)
-#struktura_now = ["CV"]
+structure_now = random.choices(structure)
+
+
 def slabika():
     syll = ""
-    if struktura_now == ["CV"]:
+    if structure_now == ["CV"]:
         syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
-        syll = syll + random.choice(vowe)
+        syll = syll + random.choices(vowe, weights = vowe_w, k=1)[0]
         return syll
     
-    elif struktura_now == ["CVCV"]:
+    elif structure_now == ["CVCV"]:
         syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
-        syll = syll + random.choice(vowe)
+        syll = syll + random.choices(vowe, weights = vowe_w, k=1)[0]
         syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
-        syll = syll + random.choice(vowe)
+        syll = syll + random.choices(vowe, weights = vowe_w, k=1)[0]
         return syll
 
-    elif struktura_now == ["CCV"]:
+    elif structure_now == ["CCV"]:
         syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
         syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
-        syll = syll + random.choice(vowe)
-        return syll
-    
-    elif struktura_now == ["CVC"]:
-        syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
-        syll = syll + random.choice(vowe)
-        syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
+        syll = syll + random.choices(vowe, weights = vowe_w, k=1)[0]
         return syll
     
-    elif struktura_now == ["CCVCC"]:
+    elif structure_now == ["CVC"]:
+        syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
+        syll = syll + random.choices(vowe, weights = vowe_w, k=1)[0]
+        syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
+        return syll
+    
+    elif structure_now == ["CCVCC"]:
         syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
         syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
-        syll = syll + random.choice(vowe)
+        syll = syll + random.choices(vowe, weights = vowe_w, k=1)[0]
         syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
         syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
         return syll
 
-
-
-
-print(slabika())
+for _ in range(10):
+    structure_now = random.choices(structure)
+    output = output + slabika() + " "
+print(output)
