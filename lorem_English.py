8ba9bc37f788d3df8a254374bb757d77a1371355
deu42236 <147750281+deu42236@users.noreply.github.com>
2023-12-11 17:25:50 +0100

    Verze 0.2 EN
    Program upřednostňuje často používaná písmena před méně častými.

--- lorem_English.py
+++ lorem_English.py
@@ -1,7 +1,11 @@
+#https://www.sttmedia.com/characterfrequency-english
+
 import random
 abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-cons = ['q', 'w', 'r', 't', 'p', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'x', 'c', 'v', 'b', 'n', 'm'] #cons... souhlasky
+cons = ['q', 'w', 'r', 't', 'p', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'x', 'c', 'v', 'b', 'n', 'm', 'th', 'he', 'an'] #cons... souhlasky
+cons_w = [9, 234, 568, 937, 166, 611, 414, 203, 192, 611, 23, 87, 424, 20, 273, 106, 154, 680, 253, 30, 30, 15]
 vowe = ['a', 'e', 'i', 'o', 'u', 'y'] #vowe... samohlasky
+vowe_w = [834, 1260, 671, 770, 285, 204]
 struktura = ["CV", "CVCV","CCV", "CVC", "CCVCC"]
 
 start = "Lorem English"
@@ -14,35 +18,35 @@ struktura_now = random.choices(struktura)
 def slabika():
     syll = ""
     if struktura_now == ["CV"]:
-        syll = syll + random.choice(cons)
+        syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
         syll = syll + random.choice(vowe)
         return syll
     
     elif struktura_now == ["CVCV"]:
-        syll = syll + random.choice(cons)
+        syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
         syll = syll + random.choice(vowe)
-        syll = syll + random.choice(cons)
+        syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
         syll = syll + random.choice(vowe)
         return syll
 
     elif struktura_now == ["CCV"]:
-        syll = syll + random.choice(cons)
-        syll = syll + random.choice(cons)
+        syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
+        syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
         syll = syll + random.choice(vowe)
         return syll
     
     elif struktura_now == ["CVC"]:
-        syll = syll + random.choice(cons)
+        syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
         syll = syll + random.choice(vowe)
-        syll = syll + random.choice(cons)
+        syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
         return syll
     
     elif struktura_now == ["CCVCC"]:
-        syll = syll + random.choice(cons)
-        syll = syll + random.choice(cons)
+        syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
+        syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
         syll = syll + random.choice(vowe)
-        syll = syll + random.choice(cons)
-        syll = syll + random.choice(cons)
+        syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
+        syll = syll + random.choices(cons, weights = cons_w, k=1)[0]
         return syll
 
 
